f9080ac03201a65dc58d3202da8ba5d8682bfd04
ualmaz <ualmaz@gmail.com>
2025-01-24 03:08:27 +0600

    translation

--- opes/forms.py
+++ opes/forms.py
@@ -12,7 +12,3 @@ class ContactForm(forms.ModelForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Email Address'}))
     phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Phone Number'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-textarea', 'placeholder': 'Your Message'}))
-
-def clean_email(self):
-        """ Allow multiple submissions with the same email. """
-        return self.cleaned_data.get('email')
--- opes/models.py
+++ opes/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Client(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    email = models.EmailField(unique=False)
+    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     message = models.TextField()
 
