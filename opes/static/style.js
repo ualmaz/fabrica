f688c3830b821af9645a975092399ee7552bf4c4
ualmaz <ualmaz@gmail.com>
2025-12-01 22:25:53 +0600

    all

---
+++ opes/static/style.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
+});
+
+
+
+
+
+
+
+
